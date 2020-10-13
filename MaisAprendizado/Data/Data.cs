1d309255f716fd9fbb076d6099418704d34406e4
Allan Da Silva Pessin <71703658+AllanPessin@users.noreply.github.com>
2020-10-13 10:34:25 -0300

    Add files via upload

---
+++ MaisAprendizado/Data/Data.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace MaisAprendizado.Data
+{
+    public class Data : IDisposable
+    {
+        //Atributo que vai permitir conectar com o Banco de dados
+        protected SqlConnection connectionDB;
+
+        //Construtor
+        public Data()
+        {
+            try
+            {
+                //string de conexão dom o Banco de Dados
+                string strConxao = @"Data source = localhost; 
+                                Initial Catalog = MaisAprendizado; 
+                                Integrated Security = true;";
+                ConnectionDB = new SqlConnection(strConxao);
+                ConnectionDB.Open();
+            }
+            catch
+            {
+
+            }
+        }
+
+        protected SqlConnection ConnectionDB { get => connectionDB; set => connectionDB = value; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
