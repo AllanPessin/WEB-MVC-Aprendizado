909b4400235db9f073619add6f995f0883feb03a
Allan Da Silva Pessin <allan.pessin@outlook.com>
2020-10-23 18:28:01 -0300

    ProfessorData - Create

--- MaisAprendizado/Data/ProfessorData.cs
+++ MaisAprendizado/Data/ProfessorData.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MaisAprendizado.Models;
 
 namespace MaisAprendizado.Data
 {
-    public class ProfessorData
+    public class ProfessorData : Data
     {
+        //Create - INSERT
+        public void Create(Pessoa pessoa)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+            cmd.CommandText = @"EXEC AdicionarAluno";
+            cmd.Parameters.AddWithValue("@Nome", pessoa.Nome);
+            cmd.Parameters.AddWithValue("@Email", pessoa.Email);
+            cmd.Parameters.AddWithValue(@"DataNascimento", pessoa.DataNascimento);
+            cmd.Parameters.AddWithValue("@Senha", pessoa.Senha);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
