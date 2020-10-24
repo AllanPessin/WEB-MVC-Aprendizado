f6f6ba466cd57359e24aa26d1bc132b784c92845
Allan Da Silva Pessin <allan.pessin@outlook.com>
2020-10-24 10:37:16 -0300

    Insert/Read - AlunoData/ProfessorData

--- MaisAprendizado/Data/AlunoData.cs
+++ MaisAprendizado/Data/AlunoData.cs
@@ -6,5 +6,27 @@ namespace MaisAprendizado.Data
 {
     public class AlunoData
     {
+        //Create - INSERT
+        public void Create(Aluno aluno)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+            cmd.CommandText = @"EXEC AdicionarAluno @Nome, @Email, @DataNascimetno, @Senha, @Telefone";
+            cmd.Parameters.AddWithValue("@Nome", aluno.Telefone);
+            cmd.ExecuteNonQuery();
+        }
+        //Read - SELECT
+        public LinkedList<Aluno> Read()
+        {
+            List<Aluno> lista = new List<Aluno>();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+            cmd.CommandText = @"SELEC * FROM Alunos ORDER BY Nome";
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Aluno aluno = new Aluno();
+            }
+        }
     }
 }
--- MaisAprendizado/Data/ProfessorData.cs
+++ MaisAprendizado/Data/ProfessorData.cs
@@ -10,25 +10,25 @@ namespace MaisAprendizado.Data
     public class ProfessorData : Data
     {
         //Create - INSERT
-        public void Create(Aluno aluno)
+        public void Create(Professor professor)
         {
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = connectionDB;
-            cmd.CommandText = @"EXEC AdicionarAluno @Nome, @Email, @DataNascimetno, @Senha, @Telefone";
-            cmd.Parameters.AddWithValue("@Nome", aluno.Telefone);
+            cmd.CommandText = @"EXEC AdicionarProfessor @Nome, @Email, @DataNascimetno, @Senha, @Credito";
+            cmd.Parameters.AddWithValue("@Nome", professor.Credito);
             cmd.ExecuteNonQuery();
         }
         //Read - SELECT
-        public LinkedList<Aluno> Read()
+        public List<Professor> Read()
         {
-            List<Aluno> lista = new List<Aluno>();
+            List<Professor> lista = new List<Professor>();
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = connectionDB;
-            cmd.CommandText = @"SELEC * FROM Alunos ORDER BY Nome";
+            cmd.CommandText = @"SELEC * FROM Professores ORDER BY Nome";
             SqlDataReader reader = cmd.ExecuteReader();
             while(reader.Read())
             {
-                Aluno aluno = new Aluno();
+                Professor professor = new Professor();
             }
         }
     }
