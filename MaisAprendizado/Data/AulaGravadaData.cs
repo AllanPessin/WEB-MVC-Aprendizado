54a20a003a7c420ac765ba44c2a0b94885b18799
Allan Da Silva Pessin <allan.pessin@outlook.com>
2020-10-30 11:57:49 -0300

    metodo Read(id) da classe AulaGravada

--- MaisAprendizado/Data/AulaGravadaData.cs
+++ MaisAprendizado/Data/AulaGravadaData.cs
@@ -14,10 +14,28 @@ namespace MaisAprendizado.Data
         {
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = connectionDB;
-            cmd.CommandText = @"INSERT INTO Aula_gravada VALUES(@AulaId, @CursoId, @Titulo,)";
+            cmd.CommandText = @"INSERT INTO Aula_gravada VALUES(@AulaId, @CursoId, @Titulo,)";//Inserir tambem no curso espcifico
             cmd.Parameters.AddWithValue("@AulaId", aula.IdAula);
             cmd.Parameters.AddWithValue("@CursoId", aula.Titulo);
             cmd.ExecuteNonQuery();
+        }
+        //Read - SELECT
+        public AulaGravada Read(int id)
+        {
+            AulaGravada aulagravada = null;
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+            cmd.CommandText = @"SELECT * FROM Aula_Gravada WHERE AulaId = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                aulagravada = new AulaGravada();
+                aulagravada.IdAula = (int)reader["AulaId"];
+                aulagravada.Titulo = (string)reader["Titulo"];
+                aulagravada.Descricao = (string)reader["Descricao"];
+            }
+            return aulagravada;
         }
     }
 }
