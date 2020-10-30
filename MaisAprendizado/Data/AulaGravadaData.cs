c25d5ac50a3430ef692a1a13074c502dc70515be
Allan Da Silva Pessin <allan.pessin@outlook.com>
2020-10-30 02:21:07 -0300

    Create(INSERT) aulagravada

--- MaisAprendizado/Data/AulaGravadaData.cs
+++ MaisAprendizado/Data/AulaGravadaData.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MaisAprendizado.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,15 @@ namespace MaisAprendizado.Data
 {
     public class AulaGravadaData : Data
     {
+        //Create - INSERT
+        public void Create(AulaGravada aula)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+            cmd.CommandText = @"INSERT INTO Aula_gravada VALUES(@AulaId, @CursoId, @Titulo,)";
+            cmd.Parameters.AddWithValue("@AulaId", aula.IdAula);
+            cmd.Parameters.AddWithValue("@CursoId", aula.Titulo);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
