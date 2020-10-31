ce2ba94e61205ee39ccc2710df0ee8151826b123
Allan Da Silva Pessin <allan.pessin@outlook.com>
2020-10-31 18:00:24 -0300

    começo do create em CompraCurso

--- MaisAprendizado/Data/CompraCursoData.cs
+++ MaisAprendizado/Data/CompraCursoData.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using MaisAprendizado.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MaisAprendizado.Data
 {
-    public class CompraCursoData
+    public class CompraCursoData : Data
     {
+        //Create - INSERT
+        public void Create(CompraCurso compracurso)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connectionDB;
+            cmd.CommandText = @"INSERT INTO Compra_Curso VALUES";
+        }
     }
 }
